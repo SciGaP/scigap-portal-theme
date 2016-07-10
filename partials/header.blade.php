7b250b4101021adcc5acda941f92c0509f32bceb
Nipurn Doshi <nipunhere@gmail.com>
2016-07-10 02:58:22 -0700

    Adding scigap theme under development

---
+++ partials/header.blade.php
@@ -0,0 +1,40 @@
+<title>Scigap</title>
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+    
+    
+    <!--[if IE 9]>
+        <script src="js/PIE_IE9.js"></script>
+    <![endif]-->
+    <!--[if lt IE 9]>
+        <script src="js/PIE_IE678.js"></script>
+    <![endif]-->
+
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+<link media="all" type="text/css" rel="stylesheet" href="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/css/style.css"/>
+<!--
+<div class="seagrid">
+    <div class="container-fluid">
+        <nav class="nav navbar-default navbar-inverse">
+            <!-- Brand and toggle get grouped for better mobile display -->
+        <!--    <div class="navbar-header page-scroll" id="home">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="{{ URL::to('/') }}/home">
+                    SCIGAP
+                </a>
+            </div>
+
+            
+            <!-- /.navbar-collapse -->
+    <!--    </nav>
+    </div>
+</div>-->
---
+++ partials/template.blade.php
@@ -0,0 +1,74 @@
+<div class="col-md-12 hero-unit scigap-top">
+	<div class="col-md-offset-9 col-md-3">
+	<ol class="breadcrumb" style="margin-top:20px;">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Team</a></li>
+	</ol>
+	</div>
+	<!--
+	<h1>Hero Image</h1>
+	<h2>This is a simple hero unit, a simple jumbotron-style component for calling extra 7
+	attention to featured content or information.</h2>
+	<p><a class="btn btn-primary btn-large">Learn more</a></p>
+	-->
+</div>
+
+<div class="col-md-offset-2 col-md-8 breathing-space seagrid-info">
+	<h1 class="text-center">What is SciGaP?</h1>
+	<p class="text-center">
+		Science Gateways are easy to build but hard to sustain. Key developers come and go. Scaling up to support growing user communities and more demanding usage takes a lot of work. Things go wrong at the worst times. SciGaP's goal is to help gateways address these problems.<br/>
+	</p>
+	<hr/>
+	<div class="col-md-5">
+	<p style="font-size:14px; margin-top:20px;">
+		In SciGaP, we assume that gateway developers understand their user communities and know the scientific capabilities that they want to provide.  Underlying these specific gateway design features are generalized services for managing applications and workflows running on remote supercomputers, as well as other services. SciGaP is a hosted service with a public API that gateways can use to outsource these general capabilities.  Gateway developers can thus concentrate their efforts on building their scientific communities and not worry about operations.  We depict this goal in the image below.
+	</p>
+	</div>
+	<div class="col-md-offset-1 col-md-5">
+	<p style="font-size:14px; margin-top:20px;">
+	Science Gateway Platform as a Service (SciGaP) provides application programmer interfaces (APIs) to hosted generic infrastructure services that can be used by domain science communities to create Science Gateways. The hosted services will support access to core infrastructure services required by Science Gateways, including: user identity, accounts, authorization, and access to multiple computational resources from campus, national, and international Grid and cloud efforts. Domain Gateway developers will be able to access these services via community-created interfaces.</p>
+
+<p>You can read our official NSF abstract here. You can read more about our vision for  Science Gateway sustainability using SciGaP and related PaaS capabilities here.</p>
+	</div>
+
+</div>
+
+
+
+<div class="col-md-12" style="background-color: #f5f7f9; padding-top:20px;">
+	<div class="col-md-offset-2 col-md-8 text-center">
+		<h1>Why use SciGaP?</h1>
+		<p class="text-center">
+			Science Gateways are virtual environments that dramatically accelerate scientific discovery by enabling scientific communities to utilize distributed computational and data resources (that is, cyberinfrastructure). Successful Science Gateways provide access to sophisticated and powerful resources, while shielding their users from the resources’ complexities.  
+		</p>
+		<hr/>
+		<div class="row" style="margin:60px 0;">
+
+			<div class="col-md-4">
+				<span class="feature-icon-img"><img src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/shield.png" width="60%"/></span>
+				<h4>Shield from complexity <br/>of resources</h4>
+			</div>
+			<div class="col-md-4">
+				<span class="feature-icon-img"><img src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/fast.png"  width="60%"/></span>
+				<h4>Accelerate scientific<br/> discovery</h4>
+			</div>
+			<div class="col-md-4">
+				<span class="feature-icon-img"><img src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/distributed.png" width="60%"/></span>
+				<h4>Access distributed<br/> data resources </h4>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="col-md-offset-2 col-md-8 text-center" style="margin-top:50px;">
+<h3>Create your Account Today!</h3>
+<button class="btn btn-primary btn-lg" style="border-radius:0; background-color:#525760; margin-top:10px;">Create Account</button>
+</div>
+
+<!--
+<div class="col-md-12 text-center blue-bg" style="padding:2.5%">
+	<a href="https://seagrid.org/jnlp/seagrid.jnlp">
+	<button style="padding:2.5%;" class="btn btn-primary col-md-offset-4 col-md-4">Download Desktop App</button>
+	</a>
+</div>
+-->
