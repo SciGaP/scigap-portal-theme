874b512d73329558f1ad7380243ca9fbf16f3599
Suresh Marru <smarru@apache.org>
2016-11-22 06:52:59 -0500

    updated title

--- partials/get-involved.blade.php
+++ partials/get-involved.blade.php
@@ -4,8 +4,8 @@
         <p><strong>SciGaP</strong> is building a hosted Platform as a Service for Science Gateways. &nbsp;The hosted service will be based on <a href="http://airavata.apache.org">Apache Airavata</a> and the Workbench Framework. Initial usage of SciGaP services will be limited to the gateways supported by the project team members before we expand to support additional gateways. However, SciGaP services will always be based on freely available open source software available through the Apache Airavata project.</p>
 		<p><strong>Other Resources:</strong></p>
 		<ul>
-			<li>Get Involved with Apache Airavata: <a href="http://airavata.apache.org/get-involved.html">http://airavata.apache.org/get-involved.html</a></li>
-			<li>Use the CIPRES Portal:&nbsp;<a href="http://www.phylo.org/">http://www.phylo.org/</a></li>
+			<li>Get Involved with Apache Airavata: <a href="https://airavata.apache.org/get-involved.html">https://airavata.apache.org/get-involved.html</a></li>
+			<li>Use the CIPRES Portal:&nbsp;<a href="https://www.phylo.org/">http://www.phylo.org/</a></li>
 			<li>Use the Neuroscience Gateway: <a href="https://www.nsgportal.org/">&nbsp;https://www.nsgportal.org/</a></li>
 			<li>Use the UltraScan Gateway: &nbsp;<a href="http://www.ultrascan.uthscsa.edu/contacts.php">http://www.ultrascan.uthscsa.edu/contacts.php</a></li>
 		</ul>
--- partials/header.blade.php
+++ partials/header.blade.php
@@ -1,4 +1,4 @@
-<title>Scigap</title>
+<title>SciGaP</title>
 <div id="navbar" class="navbar navbar-inverse">
       <div class="container-fluid">
         <div class="navbar-header">
