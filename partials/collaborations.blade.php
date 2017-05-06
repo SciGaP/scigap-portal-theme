bbf0356865bd2b177558f1482c27b75f20a68db2
Nipurn Doshi <nipunhere@gmail.com>
2017-05-06 14:09:09 -0700

    Adding Collaborations section.

---
+++ partials/collaborations.blade.php
@@ -0,0 +1,90 @@
+<div class="row breathing-space">
+      <div class="container">
+        <h1 class="breathing-space">Our Collaborators and Clients</h1>
+        <h3>Domain and Application Gateways</h3>
+        <hr>
+        <table class="col-md-12 table table-condensed breathing space">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Field of Science</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="col-md-6"><a href="https://seagrid.org/" target="_blank">SEAGrid Gateway</a></td>
+              <td  class="col-md-6">Chemistry & Engineering</td>
+            </tr>
+            <tr>
+              <td><a href="http://wiki.bcf2.uthscsa.edu/limsv3/" target="_blank">Ultrascan Gateway</a></td>
+              <td>Biophysics</td>
+            </tr>
+            <tr>
+              <td><a href="https://www.phylo.org/" target="_blank">CIPRES Gateway</a></td>
+              <td>Systematic & Population Biology</td>
+            </tr>
+            <tr>
+              <td><a href="http://www.nsgportal.org/" target="_blank">NSG Gateway</a></td>
+              <td>Neuroscience Biology</td>
+            </tr>
+            <tr>
+              <td><a href="https://testdrive.airavata.org/" target="_blank">PGA</a></td>
+              <td>Computer & Information Science & Engineering</td>
+            </tr>
+            <tr>
+              <td><a href="https://dreg.dnasequence.org/" target="_blank">dREG Gateway</a></td>
+              <td>Genetics & Nucleic Acids</td>
+            </tr>
+            <tr>
+              <td><a href="https://phasta.scigap.org/" target="_blank">Phasta Gateway</a></td>
+              <td>Mechanical Engineering</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <h3>Campus Gateways</h3>
+        <hr>
+        <table class="col-md-12 table table-condensed breathing space">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Field of Science</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><a href="https://sciencegateway.usd.edu/" target="_blank">South Dakota Gateway</a></td>
+              <td>Chemistry & Bio</td>
+            </tr>
+            <tr>
+              <td class="col-md-6"><a href="https://chemcompute.org/" target="_blank">Sonoma State College</a></td>
+              <td class="col-md-6">Chemistry</td>
+            </tr>
+            <tr>
+              <td><a href="https://cybergateway.iu.edu/" target="_blank">Indiana University Gateway</a></td>
+              <td>Generic Applications</td>
+            </tr>
+            <tr>
+              <td><a href="https://cybergateway.chpc.utah.edu" target="_blank">University of Utah</a></td>
+              <td>Chemistry</td>
+            </tr>
+            <tr>
+              <td><a href="http://sciencegateway.oscer.ou.edu/" target="_blank">Oklahoma University</a></td>
+              <td>Chemistry & Engineering</td>
+            </tr>
+            <tr>
+              <td><a href="https://scigw.oii.org/" target="_blank">Oklahoma Innovation Institute</a></td>
+              <td>Chemistry & Engineering</td>
+            </tr>
+            <tr>
+              <td><a href="http://sciencegateway.siu.edu" target="_blank">University of Southern Illinois</a></td>
+              <td>Generic Applications</td>
+            </tr>
+            <tr>
+              <td><a href="https://hpcgateway.gsu.edu/" target="_blank">Georgia State University</a></td>
+              <td>Generic Applications</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
